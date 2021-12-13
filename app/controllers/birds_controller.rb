3e2b5f44c61f9cadc4e40c9c0a28d7fccb7efb40
Garrett <garres4@vt.edu>
2021-12-13 10:49:06 -0700

    commit tree aaf85ae06f7684e231adb696c79795241795925a

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +14,11 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
